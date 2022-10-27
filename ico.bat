373766744fd9c97862b5ef958ae355cf8c117f3e
Rellik Jaeger <rellikjaeger@gmail.com>
2022-10-28 02:11:39 +0630

    Update ico.bat

--- ico.bat
+++ ico.bat
@@ -1,4 +1,23 @@
 @echo off
+setlocal enabledelayedexpansion
+
+if /i %0 equ "%~dpnx0" (
+	net session >nul 2>&1 && goto :admin
+	title Loading...
+	echo.
+	echo   Waiting for permission...
+	echo.
+	mshta "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();"
+	goto :eof
+)
+
+:admin
+net session >nul 2>&1 && (
+	title Moving ico.bat to %WinDir%
+	move /y %0 %WinDir%
+	goto :eof
+)
+
 title Converting ico files...
 
 if "[%1]" equ "[-k]" goto :k
@@ -25,6 +44,7 @@ for %%i in (*.png) do (
 	echo.
 	ffmpeg -y -i "!test!" -s 256x256 "!test:~0,-4!.ico"
 )
+title %ComSpec%
 endlocal
 goto :eof
 
@@ -52,8 +72,8 @@ for %%i in (*.png) do (
 	echo.
 	ffmpeg -y -i "!test!" -vf "scale=w=256:h=256:force_original_aspect_ratio=1,pad=256:256:(ow-iw)/2:(oh-ih)/2:color=#00000000,format=rgba" "!test:~0,-4!.ico"
 )
+title %ComSpec%
 endlocal
 
-title %ComSpec%
-
 :eof
+title %ComSpec%
